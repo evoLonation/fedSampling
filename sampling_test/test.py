e179b94b81655e4bc38bead66297ec6781137314
evoLonation <1838940019@qq.com>
2023-05-12 17:43:42 +0800

    add test.py

---
+++ sampling_test/test.py
@@ -0,0 +1,66 @@
+# coding: utf-8
+import torch
+import pickle
+import sys
+import numpy as np
+sys.path.append("..")
+from sampling_test.read_db import get_dataloaders
+from py_func.FedProx import accuracy_dataset
+from py_func.create_model import NN
+from torch.utils.data import DataLoader
+from torchvision.datasets import MNIST
+import torchvision.transforms as transforms
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+print("Experiment running on device: {}".format(device))
+#################  加载模型  #################
+dataset = sys.argv[1]
+isiid = sys.argv[2] == "True"
+if dataset == "mnist" :
+	model = NN(50, 10)
+	if isiid:
+		state_dict = torch.load("../saved_exp_info/final_model/MNIST_iid_random_any_i600_N50_lr0.01_B50_d1.0_p0.1_m2_0.pth")
+	else:
+		state_dict = torch.load("../saved_exp_info/final_model/MNIST_shard_random_any_i600_N50_lr0.01_B50_d1.0_p0.1_m2_0.pth")
+elif dataset == "cifar":
+	model = CNN_CIFAR_dropout()
+	if isiid:
+		state_dict = torch.load("../saved_exp_info/final_model/CIFAR10_iid_0.01_random_any_i1000_N100_lr0.05_B50_d1.0_p0.1_m5_0.pth")
+	else:
+		state_dict = torch.load("../saved_exp_info/final_model/CIFAR10_nbal_0.01_random_any_i1000_N100_lr0.05_B50_d1.0_p0.1_m5_0.pth")
+
+model.load_state_dict(state_dict)
+
+model.to(device=device)
+
+model.eval()
+
+
+#################  加载测试集，打开文件  #################
+
+if dataset == "mnist" :
+	if isiid:
+		file = open("saved_exp_info/MNIST_iid.txt", "w")
+		list_ds_test = get_dataloaders("MNIST_iid", 50)
+	else:
+		file = open("saved_exp_info/MNIST_shard.txt", "w")
+		list_ds_test = get_dataloaders("MNIST_shard", 50)
+elif dataset == "cifar":
+	if isiid:
+		file = open("saved_exp_info/CIFAR10_iid.txt", "w")
+		list_ds_test = get_dataloaders("CIFAR10_iid", 50)
+	else:
+		file = open("saved_exp_info/CIFAR10_nbal_0.001.txt", "w")
+		list_ds_test = get_dataloaders("CIFAR10_nbal_0.001", 50)
+
+
+for (i, ds) in enumerate(list_ds_test):
+	file.write(f"client {i}\n")
+	print(f"client {i}")
+	for i in range(100):
+		new_indices = np.random.choice(len(ds), size=len(ds), replace=True)
+		sampled_ds = [ds[i] for i in new_indices]
+		dl = DataLoader(sampled_ds, batch_size=200, shuffle=True)
+		acc = accuracy_dataset(model, dl)
+		file.write(f"{acc}\n")
+		print(acc)
--- sampling_test/test_mnist.py
+++ sampling_test/test_mnist.py
@@ -4,6 +4,8 @@ import pickle
 import sys
 import numpy as np
 sys.path.append("..")
+from py_func.FedProx import accuracy_dataset
+from py_func.read_db import get_dataloaders
 from py_func.create_model import NN
 from torch.utils.data import DataLoader
 from torchvision.datasets import MNIST
@@ -32,19 +34,11 @@ _, list_mnist_test = get_dataloaders("MNIST_shard", 50)
 
 
 for i in range(1000):
-	new_indices = np.random.choice(len(mnist_test), size=len(mnist_test), replace=True)
-	sampled_trainset = [mnist_test[i] for i in new_indices]
-	test_loader = DataLoader(sampled_trainset, batch_size=50, shuffle=True)
-
-	correct = 0
-	for features, labels in test_loader:
-		predictions = model(features) # 对应注释中，我们根据feature和model，来得出label的prediction
-		_, predicted = predictions.max(1, keepdim=True)  # 我明白了，因为.max函数输出的是两个，第一个是每一行的最大值，第二个是每一行中最大值所在的位置，而我们需要的是位置吧，所以前面的“_”仅仅是用于占位的。我们需要的是每一行中最大值所在的位置的索引号  
-		# 这个Keepdim=True仅仅是为了维持其二维特性，即对应位置操作。
-		# print(predicted.view(-1, 1))
-		# print(labels.view(-1, 1))
-		correct += torch.sum(predicted.view(-1, 1) == labels.view(-1, 1)).item()  # 首先，这个view(-1,1) 是分别让predicted和view这两个array按每一行从左到右的顺序，按列进行排列，然后两列数据，进行个对比。计算出相等的个数，然后加起来。
-		# print(correct)
-	accuracy = 100 * correct / len(test_loader.dataset)  # 计算模型在测试数据集上的准确度。
-	# print(f"====> Accuracy: {accuracy}")
-	print(accuracy)
+	new_indices = np.random.choice(len(list_mnist_test), size=len(list_mnist_test), replace=True)
+	list_sampled_test = [list_mnist_test[i] for i in new_indices]
+	test_loader = DataLoader(list_sampled_test, batch_size=50, shuffle=True)
+	acc_hist = np.zeros(100)
+	for k, dl in enumerate(list_sampled_test):
+		acc_hist[k] = accuracy_dataset(model, dl)
+	server_acc = np.mean(acc_hist)
+	print(server_acc)
