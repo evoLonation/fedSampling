419f5b6ce0cd15a9645607b992e4c3de73bf65de
evoLonation <1838940019@qq.com>
2023-05-13 20:35:26 +0800

    对iid的进行总体抽样

--- sampling_test/test.py
+++ sampling_test/test.py
@@ -58,19 +58,41 @@ elif dataset == "cifar":
 		file2 = open("saved_exp_info/CIFAR10_nbal_0.001_python.txt", "w")
 		list_ds_test = get_dataloaders("CIFAR10_nbal_0.001", 50)
 
-# if dataset == "cifar" and isiid:
-file2.write("datasets = [\n")
-for (i, ds) in enumerate(list_ds_test):
-	file2.write("[")
-	file.write(f"client {i}\n")
-	print(f"client {i}")
-	for j in range(100):
-		new_indices = np.random.choice(len(ds), size=len(ds), replace=True)
-		sampled_ds = [ds[k] for k in new_indices]
-		dl = DataLoader(sampled_ds, batch_size=200, shuffle=True)
-		acc = accuracy_dataset(model, dl)
-		file.write(f"{acc}\n")
-		print(acc)
-		file2.write(f"{acc}, ")
-	file2.write("],\n")
-file2.write("]\n")
+if isiid:
+	file2.write("datasets = [\n")
+	for i in range(100):
+		file2.write("[")
+		file.write(f"client {i}\n")
+		print(f"client {i}")
+		for _ in range(100):
+			new_indices1 = np.random.choice(len(list_ds_test), size=20, replace=True)
+			sample_list_ds_test = [list_ds_test[k] for k in new_indices1]
+			acc_list = []
+			for j in range(20):
+				ds_test = sample_list_ds_test[j]
+				new_indices2 = np.random.choice(len(ds_test), size=50, replace=True)
+				sample_ds_test = [ds_test[k] for k in new_indices2]
+				dl = DataLoader(sample_ds_test, batch_size=200, shuffle=True)
+				acc_list += [accuracy_dataset(model, dl)]
+			acc = np.mean(np.array(acc_list))
+			file.write(f"{acc}\n")
+			print(acc)
+			file2.write(f"{acc}, ")
+		file2.write("],\n")
+	file2.write("]\n")
+else:
+	file2.write("datasets = [\n")
+	for (i, ds) in enumerate(list_ds_test):
+		file2.write("[")
+		file.write(f"client {i}\n")
+		print(f"client {i}")
+		for j in range(100):
+			new_indices = np.random.choice(len(ds), size=len(ds), replace=True)
+			sampled_ds = [ds[k] for k in new_indices]
+			dl = DataLoader(sampled_ds, batch_size=200, shuffle=True)
+			acc = accuracy_dataset(model, dl)
+			file.write(f"{acc}\n")
+			print(acc)
+			file2.write(f"{acc}, ")
+		file2.write("],\n")
+	file2.write("]\n")
